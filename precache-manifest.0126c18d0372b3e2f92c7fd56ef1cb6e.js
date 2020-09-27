8f7cd5700f704080f50f606656a9a93bb43ee9ae
Dmitriy Brovchenko <dbrov@mail.ru>
2020-09-27 21:25:27 +0300

    Updates

---
+++ precache-manifest.0126c18d0372b3e2f92c7fd56ef1cb6e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0ec9753fea676aecccef295779fc37",
+    "url": "/okswes.github.io/react-mesto-auth/index.html"
+  },
+  {
+    "revision": "e23ee7b9522f93ca48c6",
+    "url": "/okswes.github.io/react-mesto-auth/static/css/main.6bd32989.chunk.css"
+  },
+  {
+    "revision": "20f45f5cb5404e64b69f",
+    "url": "/okswes.github.io/react-mesto-auth/static/js/2.fe0cafd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/okswes.github.io/react-mesto-auth/static/js/2.fe0cafd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23ee7b9522f93ca48c6",
+    "url": "/okswes.github.io/react-mesto-auth/static/js/main.ad8dc0fb.chunk.js"
+  },
+  {
+    "revision": "9b4c78f2d030ed694e4b",
+    "url": "/okswes.github.io/react-mesto-auth/static/js/runtime-main.51b8a4e6.js"
+  },
+  {
+    "revision": "1b13e59e42978eef01d976fb2808e41c",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Black.1b13e59e.woff"
+  },
+  {
+    "revision": "44b1541a96341780b29112665c66ac67",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Black.44b1541a.otf"
+  },
+  {
+    "revision": "a33f14083a11e8c4690c518eb4dce28e",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Black.a33f1408.ttf"
+  },
+  {
+    "revision": "a6f62b1ef31366a1a4f8e6a7e0a1ece5",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Black.a6f62b1e.woff2"
+  },
+  {
+    "revision": "1f3fcaf555abac660cc9ad01698f7d12",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Medium.1f3fcaf5.woff"
+  },
+  {
+    "revision": "1f56c18a246ac983b3aea635cdbd6ad9",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Medium.1f56c18a.otf"
+  },
+  {
+    "revision": "4401ca19be5f325e28ee45d96516a35e",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Medium.4401ca19.ttf"
+  },
+  {
+    "revision": "64ff83f0ad45124de98dcb9102ff8736",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Medium.64ff83f0.woff2"
+  },
+  {
+    "revision": "5c522d2004c3279549bd88222823776f",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-Regular.5c522d20.otf"
+  },
+  {
+    "revision": "3256632322976f3adb8b55a88bfe655b",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-SemiBold.32566323.woff2"
+  },
+  {
+    "revision": "6e41db692f25a97b37cdb2a86a88b0f8",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-SemiBold.6e41db69.ttf"
+  },
+  {
+    "revision": "a49c9f509cf05c28f368e614c1fc41d9",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-SemiBold.a49c9f50.woff"
+  },
+  {
+    "revision": "ba72a985a4994051b59e2b402d30aa82",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/Inter-SemiBold.ba72a985.otf"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/NotLogin.4072126c.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/OkLogin.e38b395c.svg"
+  },
+  {
+    "revision": "9e35f85831d68caadf15de3f2c7450fd",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/addbutton.9e35f858.svg"
+  },
+  {
+    "revision": "33286c6dc65271f0de96dbd3742f11a6",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/addbutton2.33286c6d.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/avatarEdit.d476a8b8.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/deleteIcon.d8078343.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/editbutton.b05083e5.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/like.a8acc871.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/liked.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/okswes.github.io/react-mesto-auth/static/media/logo.c6f11019.svg"
+  }
+]);
